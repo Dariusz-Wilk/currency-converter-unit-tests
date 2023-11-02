bbd2ac158b921c327bfe2cbf5efd665accc4eef5
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-02 14:14:38 +0000

    Added testCases array with different cases to test

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,42 +1,54 @@
-import { render, screen, type, selectOptions } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
 
 import userEvent from '@testing-library/user-event';
 
-describe('Component CurrencyForm', () => {
-	it('should render without crashing', () => {
-		render(<CurrencyForm action={() => {}} />);
-	});
+const testCases = [
+	{ amount: '100', from: 'PLN', to: 'USD' },
+	{ amount: '20', from: 'USD', to: 'PLN' },
+	{ amount: '200', from: 'PLN', to: 'USD' },
+	{ amount: '345', from: 'USD', to: 'PLN' },
+];
 
-	it('should run action callback with proper data on form submit', () => {
-		// create fake action function
-		const action = jest.fn();
+for (const test of testCases) {
+	describe('Component CurrencyForm', () => {
+		it('should render without crashing', () => {
+			render(<CurrencyForm action={() => {}} />);
+		});
 
-		// render component
-		render(<CurrencyForm action={action} />);
+		it('should run action callback with proper data on form submit', () => {
+			// create fake action function
+			const action = jest.fn();
 
-		// find “convert” button
-		const submitButton = screen.getByText('Convert');
+			// render component
+			render(<CurrencyForm action={action} />);
 
-		//find fields elems
-		const amountField = screen.getByTestId('amount');
-		const fromField = screen.getByTestId('from');
-		const toField = screen.getByTestId('to');
+			// find “convert” button
+			const submitButton = screen.getByText('Convert');
 
-		// set test values to fields
-		userEvent.type(amountField, '100');
-		userEvent.selectOptions(fromField, 'PLN');
-		userEvent.selectOptions(toField, 'USD');
+			//find fields elems
+			const amountField = screen.getByTestId('amount');
+			const fromField = screen.getByTestId('from');
+			const toField = screen.getByTestId('to');
 
-		// simulate user click on "convert" button
-		userEvent.click(submitButton);
+			// set test values to fields
+			userEvent.type(amountField, test.amount);
+			userEvent.selectOptions(fromField, test.from);
+			userEvent.selectOptions(toField, test.to);
 
-		// check if action callback was called once and with proper argument
-		expect(action).toHaveBeenCalledTimes(1);
-		expect(action).toHaveBeenCalledWith({
-			amount: 100,
-			from: 'PLN',
-			to: 'USD',
+			// simulate user click on "convert" button
+			userEvent.click(submitButton);
+
+			// check if action callback was called once and with proper argument
+			expect(action).toHaveBeenCalledTimes(1);
+			expect(action).toHaveBeenCalledWith({
+				amount: +test.amount,
+				from: test.from,
+				to: test.to,
+			});
 		});
 	});
-});
+
+	// unmount component
+	cleanup();
+}
